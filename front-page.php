fe13a1d999fe793c0994ab8799e821992d113ac4
Ambre hp <ambre.haphuoc@gmail.com>
2024-02-23 18:13:30 +0100

    intégration liste photo

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@
 ?>
 <?php get_header(); ?>
 
-<!-- Wrap Slider Area -->
+<!-- Intégration du random hero -->
 <div class="hero-container">
     <div class="hero-photo">
         <!-- Initialisation de post à afficher -->
@@ -37,6 +37,42 @@
         <h1 class="hero-title">Photographe Event</h1>
     </div>
 </div>
+
+<!-- Intégration liste photo -->
+<div class="photo-grid">
+    <?php
+    $args = array(
+        'post_type' => 'photo',
+        'posts_per_page' => 16,
+        'order' => 'ASC',
+        'orderby' => 'date',
+    );
+
+    $query_grid = new WP_Query($args);
+
+    if ($query_grid->have_posts()) {
+        $count = 0;
+        while ($query_grid->have_posts()) {
+            $query_grid->the_post();
+            $count++;
+    ?>
+
+            <!-- Affiche les photos dynamiquement -->
+            <?php get_template_part('templates/photo-block'); ?>
+    <?php
+        }
+    }
+    ?>
+</div>
+
+<div class="load-more-container">
+    <div class="load-more">
+        <button class="btn-more" data-page="2" data-max-pages="<?php echo $query_grid->max_num_pages; ?>" data-nonce="<?php echo wp_create_nonce('load_more_photos'); ?>">
+            Charger plus
+        </button>
+    </div>
+</div>
+
 <?php
 // On réinitialise à la requête principale
 wp_reset_postdata();
